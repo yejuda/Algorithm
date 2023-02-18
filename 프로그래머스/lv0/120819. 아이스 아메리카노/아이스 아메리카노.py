4064684a9d3e4683c33817ac36e06cdf9e715ad6
yejuda <110321222+yejuda@users.noreply.github.com>
2023-02-18 16:11:23 +0900

    Update 아이스 아메리카노.py

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,4 +1,7 @@
+# def solution(money):
+#     americano = money // 5500
+#     left = money - (americano * 5500)
+#     return [americano, left]
+
 def solution(money):
-    americano = money // 5500
-    left = money - (americano * 5500)
-    return [americano, left]
+    return [money // 5500, money % 5500]
