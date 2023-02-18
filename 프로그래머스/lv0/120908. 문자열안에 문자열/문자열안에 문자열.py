ff1e3218de7aaf60de8c94fffef46de930ca2a6a
yejuda <110321222+yejuda@users.noreply.github.com>
2023-02-18 22:31:16 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -0,0 +1,10 @@
+# def solution(str1, str2):
+#     answer = 0
+#     if str2 in str1:
+#         answer = 1
+#     else:
+#         answer = 2
+#     return answer
+
+def solution(str1, str2):
+    return 1 if str2 in str1 else 2
