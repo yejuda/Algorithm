4eaf6e27788b6a5c3accb180e29dc8a5541fe1b0
yejuda <110321222+yejuda@users.noreply.github.com>
2023-02-18 21:44:53 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,2 @@
 def solution(s1, s2):
-    answer = 0
-    for i in s1:
-        for j in s2:
-            if i == j:
-                answer += 1
-    return answer
+    return len(set(s1).intersection(set(s2)))
