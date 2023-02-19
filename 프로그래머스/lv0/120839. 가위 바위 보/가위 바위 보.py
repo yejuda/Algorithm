951440b828d998556cc53ae8cdb4f198c55ad732
yejuda <110321222+yejuda@users.noreply.github.com>
2023-02-19 16:26:00 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,11 +1,15 @@
-def solution(rsp):
-    answer = ''
-    for i in rsp:
-        if i == '2':
-            answer += '0'
-        elif i == '0':
-            answer += '5'
-        else:
-            answer += '2'
+# def solution(rsp):
+#     answer = ''
+#     for i in rsp:
+#         if i == '2':
+#             answer += '0'
+#         elif i == '0':
+#             answer += '5'
+#         else:
+#             answer += '2'
             
-    return answer
+#     return answer
+
+def solution(rsp):
+    d = {'0':'5','2':'0','5':'2'}
+    return ''.join(d[i] for i in rsp)
