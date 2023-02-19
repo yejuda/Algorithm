6dab7fa30490d401599136f157d2e1a7e2a8233d
yejuda <110321222+yejuda@users.noreply.github.com>
2023-02-19 16:31:31 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,13 @@
+# def solution(my_string):
+#     answer = ''
+#     for i in my_string:
+#         if i.isupper():
+#             answer += i.lower()
+#         else:
+#             answer += i.upper()
+            
+#     return answer
+
+def solution(my_string):
+    return my_string.swapcase()
+
