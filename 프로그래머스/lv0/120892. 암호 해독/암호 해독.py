6841d53d1bd81ceff06f2f17965d379e3d990ed0
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2023-02-21 19:33:07 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,7 +1,11 @@
-def solution(cipher, code):
-    answer = ''
+# def solution(cipher, code):
+#     answer = ''
     
-    for i in range(code-1, len(cipher), code):
-        answer += cipher[i]
+#     for i in range(code-1, len(cipher), code):
+#         answer += cipher[i]
 
-    return answer
+#     return answer
+
+def solution(cipher, code):
+
+    return cipher[code-1::code]
