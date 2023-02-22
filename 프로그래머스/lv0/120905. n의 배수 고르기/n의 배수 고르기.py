2b578647d950f5679c4075a82188bc658c3e2719
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2023-02-22 13:18:45 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -0,0 +1,9 @@
+# def solution(n, numlist):
+#     answer = []
+#     for i in numlist:
+#         if i % n == 0:
+#             answer.append(i)
+#     return answer
+
+def solution(n, numlist):
+    return [i for i in numlist if i % n == 0]
