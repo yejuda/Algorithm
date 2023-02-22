364ab7a074c272410b49c52db6af2240c871edde
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2023-02-22 11:48:54 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,10 +1,13 @@
-def solution(my_string):
-    answer = []
+# def solution(my_string):
+#     answer = []
     
-    for i in my_string:
+#     for i in my_string:
         
-        if i.isdigit():
-            answer.append(int(i))
-            answer.sort()
+#         if i.isdigit():
+#             answer.append(int(i))
+#             answer.sort()
             
-    return answer
+#     return answer
+
+def solution(my_string):
+    return sorted([int(i) for i in my_string if i.isdigit()])
