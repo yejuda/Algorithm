58c0e033c3dad5aa23a37d998397a888ef28ad72
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2023-02-23 23:20:48 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,8 +1,15 @@
-from string import ascii_lowercase
+# from string import ascii_lowercase
+
+# def solution(age):
+#     answer = ''
+#     alph_list = list(ascii_lowercase)
+#     for i in str(age):
+#         answer += alph_list[int(i)]
+#     return answer
 
 def solution(age):
     answer = ''
-    alph_list = list(ascii_lowercase)
+    alph_list = ['a','b','c','d','e','f','g','h','i','j']
     for i in str(age):
         answer += alph_list[int(i)]
     return answer
