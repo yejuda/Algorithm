f521faa6fb59d46a9b10bd7b98a020d4d11d3e27
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2023-02-26 10:46:45 +0900

    [level 0] Title: 약수 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
@@ -1,7 +1,10 @@
-def solution(n):
-    answer = []
-    for i in range(1, n+1):
+# def solution(n):
+#     answer = []
+#     for i in range(1, n+1):
         
-        if n % i == 0:
-            answer.append(i)
-    return answer
+#         if n % i == 0:
+#             answer.append(i)
+#     return answer
+
+def solution(n):
+    return [i for i in range(1, n+1) if n % i == 0]
