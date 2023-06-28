9c96c5ecc8c677cd21a7cf4334a03505ce3250f0
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2023-06-28 22:35:48 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,7 +1,11 @@
-def solution(seoul):
-    answer = ''
-    for i in range(len(seoul)):
-        if seoul[i] == 'Kim':
-            answer = "김서방은 {}에 있다".format(i)
+# def solution(seoul):
+#     answer = ''
+#     for i in range(len(seoul)):
+#         if seoul[i] == 'Kim':
+#             answer = "김서방은 {}에 있다".format(i)
 
-    return answer
+#     return answer
+
+def solution(seoul):
+
+    return "김서방은 {}에 있다".format(seoul.index("Kim"))
