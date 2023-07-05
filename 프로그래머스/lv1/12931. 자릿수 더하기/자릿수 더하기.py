847aee463230fa409d54a9f25ab2d61011e74a3f
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2023-07-05 21:34:42 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,13 @@
+# def solution(n):
+#     answer = 0
+#     N = []
+
+#     for i in str(n):
+#         N.append(int(i))
+#     return sum(N)
+
+def solution(n):
+
+    N = [int(i) for i in str(n)]
+    
+    return sum(N)
