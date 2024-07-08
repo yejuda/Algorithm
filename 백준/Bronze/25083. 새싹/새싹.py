1c22819de8743e236c4d08f4d20c2b0a4f8b4b29
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2024-07-08 21:07:47 +0900

    [Bronze V] Title: 새싹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,6 @@
+print("         ,r\'\"7", end= '\n')
+print("r`-_   ,'  ,/", end= '\n')
+print(" \. \". L_r'", end= '\n')
+print("   `~\/", end= '\n')
+print("      |", end= '\n')
+print("      |", end= '\n')
