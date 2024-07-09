1570596392d3a70e63e7462575fff17a614326af
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2024-07-09 15:17:03 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,10 +1,6 @@
-import sys
+# 줄을 바꿔가면서 입력 받기
+a = [int(input()) for _ in range(9)]
 
-# 전체 입력을 받아서 처리
-input_data = sys.stdin.read().strip().split()
-
-# 문자열 리스트를 정수 리스트로 변환
-a = list(map(int, input_data))
 
 print(max(a))
 print(a.index(max(a))+1)
