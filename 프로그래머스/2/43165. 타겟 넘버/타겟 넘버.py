5c543aff770c7bf2c07115338c48dd92a35fe8eb
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2024-07-21 00:12:58 +0900

    [level 2] Title: 타겟 넘버, Time: 363.41 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,24 @@
+# dfs
+
 cnt = 0
 
 def solution(numbers, target):
+    
     dfs(numbers, target, 0, 0)
+    
     return cnt
 
-
-def dfs(numbers, target, curr, idx):
+def dfs(numbers, target, curr, idx):  # curr: 현재까지 연산한 값
     global cnt
     
-    # idx의 역할 : 우리는 numbers의 크기만큼 반복을 해야하기 때문에 그것을 count해주기 위한 역할이다.
-    if idx == len(numbers):  # 인덱스를 하나씩 증가시킬 것. 그리고 이 인덱스가 numner의 크기와 같다면
-        if curr == target:  # 그리고 현재 값이랑 target이 같다면
-            cnt += 1        # cnt(target값 나온 경우의 수) 증가
+    if len(numbers) == idx:
+        if target == curr:
+            cnt += 1 
         return
             
-    # 재귀함수
-    dfs(numbers, target, curr + numbers[idx], idx+1)
-    dfs(numbers, target, curr - numbers[idx], idx+1)
+    dfs(numbers, target, curr+numbers[idx], idx+1)  # curr(현재값)과 idx를 업데이트 -> 재귀를 통해 반복
+    dfs(numbers, target, curr-numbers[idx], idx+1)  # 이건 뺏을 경우
+            
+    
+
     
