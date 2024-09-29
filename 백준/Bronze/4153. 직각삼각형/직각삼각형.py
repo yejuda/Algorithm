b35b501cfff61ba8113efad6827188edd8e577af
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2024-09-29 22:22:28 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,8 +1,9 @@
 while True:
-    a, b, c = map(int, input().split())
+    a,b,c = map(int, input().split())
     if a == 0 and b == 0 and c == 0:
         break
-    if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
+    # 세변 중 어느 변이든 빗변이 될 수 있기 때문에 a,b,c 각각이 빗변인 경우 구하기
+    elif a**2 + b**2 == c**2 or b**2 + c**2 == a**2 or a**2 + c**2 == b**2:
         print("right")
-    else: 
+    else:
         print("wrong")
