00155695700c561132d48216b8e163c8a33d045b
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2024-12-27 21:46:08 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,10 +1,5 @@
 def solution(spell, dic):
-    # for i in dic:
-    #     for j in spell: 
-    #         if i in j:   # dic의 단어들 안에 spell이 있다면 
-    #             return 1
-    #         else:
-    #             return 2
+
     spell_x = ''.join(sorted(spell))
     
     for i in dic:
