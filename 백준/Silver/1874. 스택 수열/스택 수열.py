4c5c4a04c1a08145e9bd59cba914cb87c18caf17
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-05-06 10:37:30 +0900

    [Silver II] Title: 스택 수열, Time: 2540 ms, Memory: 35020 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,32 +1,27 @@
-# 수를 스택에 뽑아 늘어놓음으로써 하나의 수열을 만들 수 있다. 
-# 스택에 push하는 순서는 반드시 오름차순(1부터 n까지)을 지킨다.
-# 1부터 n까지의 숫자를 넣었다 뺐다 하면서 최종적으로 스택이 비게 되는지 아닌지 확인
 n = int(input())
-st = []
-result = []  # 출력 결과를 담을 곳
-cnt = 1 # 1부터 n까지 push할 숫자
 
+stack = []
+result = []
+cnt = 1
 
 for _ in range(n):
-    # 수 입력 받기
     m = int(input())
 
-
-    # m이 나올 때까지 push
     while cnt <= m:
-        st.append(cnt)
-        result.append('+')
+        stack.append(cnt)
+        result.append("+")
         cnt += 1
-    
-    # 이제 스택 top이 m과 같아야 pop가능!
-    if st[-1] == m:
-        st.pop()
+
+    # 스택의 탑이랑 입력 수가 같으면 계속 pop해야 하기 때문에 break를 하면 X
+    if stack[-1] == m:
+        stack.pop()
         result.append('-')
-    # 스택의 top이 m과 같지 않다면 수열이 아니다! 같아야 pop을 할 수 있기 때문!!
+    
     else:
         print("NO")
         break
 
+# break문으로 for문을 빠져나오더라도 실행되는 것을 막기 위해 
 else:
-    for r in result:
-        print(r)
+    for k in result:
+        print(k)
