bc9dac496ff430422952e5cb5f60b498b18538c6
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-05-07 10:46:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 112 ms, Memory: 110944 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,31 +1,30 @@
-# 주어진 각 랜선을 같은 길이로 잘라야 한다.
-# 합이 목표 개수 n이상인지 확인 -> 이분 탐색 조건
+# 이분 탐색으로 풀어야 해.
 
-# 이미 가지고 있는 랜선개수, 필요한 랜선 개수
 k, n = map(int, input().split())
 lines = [int(input()) for _ in range(k)]
 
-# 초기 범위 설정
 start = 1
 end = max(lines)
 result = 0  # 만들 수 있는 최대 길이 저장
 
-# start가 더 크면 더이상 가능한 길이가 없음을 뜻한다.
-while start <= end:
-    # 중간 길이 설정
+
+# 계속 잘라가면서 비교
+while start <= end:   # start가 더 작거나 같을 때까지 반복(그게 넘어가면 비교할 필요 없음 start가 더 클 수 없음)
+    # 중간 길이 구하기
     mid = (start + end) // 2
 
     cnt_line = 0
 
+    # 길이를 돌아가면서 저장
     for line in lines:
         cnt_line += (line // mid)
 
-    # 더 긴 길이로 자를 수 있는 경우
+    # 더 긴 길이로 자를 수 있는 경우(현재는 길이가 짧기 떄문에 cnt_line이 개수가 더 많아진 것이다.)
     if cnt_line >= n:
-        result = mid  # 가능한 길이 중 최대를 저장(계속 갱신)
-        start = mid + 1  # 더 길게 해도 되나? 확인
-    # (cnt_line < n) -> 너무 많이 자른 경우
-    else: 
-        end = mid - 1
+        result = mid
+        start = mid+1
+    # 너무 많이 자른 경우
+    else:
+        end = mid-1
 
 print(result)
