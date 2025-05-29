61d3e77c47bc6cea6ca691dc517a99852d07f811
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-05-29 12:37:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 308 ms, Memory: 36016 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,64 +1,70 @@
-from collections import deque
+## 단, 방문할 수 있는 정점이 여러 개인 경우에는,
+## 정점 번호가 작은 것을 먼저 방문(set을 활용하는 방법도 있음)
+## 1. set은 중복을 허용하지 않는다. 2. 자동 정렬된다.(순서가 없음)
 
-# 정점이 작은 것을 먼저 방문해야 하기 때문에 list가 아닌 set을 사용
-# 1. set은 중복을 허용하지 않는다. 2. 자동 정렬된다(순서가 없음)
+n, m, v = map(int, input().split()) 
 
-# 정점개수, 간선개수, 탐색시작 정점 번호 입력 받기
-n, m, s = map(int, input().split())
+# 방문 확인 변수(dfs, bfs따로 설정해두어야 함.)
+visited1 = [False] * (n+1)  # dfs
+visited2 = [False] * (n+1)  # bfs
 
-# 방문 확인 변수
-visited1 = [False]*(n+1)   # dfs
-visited2 = [False]*(n+1)   # bfs
-
-# 그래프 초기화
+# 노드 정보(그래프 구조)
+# 노드 개수만큼 생성되고 그 안에 노드와 연결 되어 있는 정보가 들어간다
 graph = [[] for _ in range(n+1)]
 
-# 간선 연결 정보 받기
-# 그래프에 노드와 간선 정보 넣기
-for _ in range(m):   # 간선 수만큼 반복
-    # 노드와 간선 정보 입력받기
+# 노드 정보 입력해주기 
+for _ in range(m): # 간선 수만큼
     a, b = map(int, input().split())
     graph[a].append(b)
-    graph[b].append(a)  # 양방향
+    graph[b].append(a)   # 양방향 연결이기 때문에!
 
-# 각 리스트를 정렬하여 작은 정점을 먼저 방문하도록 설정
-for edges in graph:
-    edges.sort()
+## 정렬
+# 정점 번호가 작은 것을 먼저 방문해야 하기 때문에 '정렬' 해주기
+for s in graph:
+    s.sort()
 
+# dfs 함수 생성
+def dfs(v):
 
-# BFS #### deque, while
-def bfs(s):
-    # 방문한 곳을 순서대로 넣을 큐
-    q = deque([s])  # 첫번째 노드 넣음
-
-    # 방문 처리
-    visited2[s] = True
-    
-    while q:  # 큐가 비어있을 때까지
-        # 왼쪽에 있는 값 pop
-        s_pop = q.popleft()
-        print(s_pop, end = ' ')
-
-        for i in graph[s_pop]:  # pop한 노드와 연결되어 있는 노드 가져오기
-            if not visited2[i]:
-                q.append(i) # 큐에 추가
-                visited2[i] = True # 방문처리
-
-
-# DFS
-def dfs(s):
-    # 방문처리
-    visited1[s] = True
+    # 현재 노드 방문 처리
+    visited1[v] = True
     # 방문 후 정점 출력
-    print(s, end= ' ')
-    
-    # 현재 노드에 연결되어 있는 노드 탐색하면서 방문처리
-    for i in graph[s]:  # s번 노드에 연결된 모든 노드를 반복
-        if not visited1[i]: # 방문하지 않았다면
-            # 재귀를 통해 방문처리
-            dfs(i)
+    print(v, end = ' ')
+
+    for neighbor in graph[v]:  # 그래프를 순회하기(현재 노드와 연결되어 있는 노드들을 순회해야 함)
+        if not visited1[neighbor]:     # 만약 아직 방문하지 않은 노드라면
+            dfs(neighbor)  # 재귀적으로 방문 처리
 
 
-dfs(s)
+## BFS(deque)
+from collections import deque
+
+# bfs
+def bfs(v):
+
+    # 큐에 첫 노드 넣기
+    queue = deque([v])
+    # 첫노드 방문처리
+    visited2[v] = True
+
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 노드 빼서 탐색
+        node = queue.popleft()
+        # 방문 후 정점 출력
+        print(node, end=' ')
+
+        for neighbor in graph[node]:
+            if not visited2[neighbor]:
+                # 방문처리
+                visited2[neighbor] = True
+                # 해당 노드 다시 큐에 넣기(이후 해당 노드와 연결되어 있는 노드들을 탐색하기 위해서)
+                queue.append(neighbor)
+
+
+
+# 1번 컴퓨터와 연결되어 있는 컴퓨터 수를 알기 위해서는 
+# 1번 노드부터 시작했을 때 방문하는 컴퓨터 개수 
+dfs(v)
 print()
-bfs(s)
+bfs(v)
