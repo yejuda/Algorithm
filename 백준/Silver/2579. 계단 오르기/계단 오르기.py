a530f8362456def60be4a5a26b0d47e01eda86f9
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-06-12 12:28:28 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,26 +1,25 @@
 # "게임에서 얻을 수 있는 총 점수의 최댓값"
 # 다이나믹 프로그래밍
+# 가능한 경로 중 최대 점수 얻는 방법을 dp로 차근차근 쌓아감
 
 n = int(input())
 
-# 계단 점수 입력
-scores = [int(input()) for _ in range(n)]
+# 점수 입력 받기
+score = [int(input()) for _ in range(n)]
 
-# 1. dp 테이블를 정의한다.
-dp = [0] * (n + 1)
+# dp초기화
+dp = [0] * (n)
 
+if n <= 2:  # 계단이 2개 이하일땐 그냥 다 더해서 출력
+    print(sum(score))  
 
-if n == 1:
-    print(scores[0])
-elif n == 2:
-    print(scores[0] + scores[1])
+# 3개 이상인 경우
 else:
-    # 2️. dp 테이블의 초기값 계산한다.
-    dp[1] = scores[0]
-    dp[2] = scores[0] + scores[1]
-    # 3️. 점화식에 따라 각 계단에 대한 dp 테이블을 채워준다.
-    for i in range(3, n+1):
-        dp[i] = max(scores[i-1] + dp[i-2], dp[i-3] + scores[i-2] + scores[i-1])
+    # 1번째, 2번째 계단 초기화
+    dp[0] = score[0]
+    dp[1] = score[0] + score[1]
 
-    # 4️. dp 테이블의 마지막 값을 출력한다.
+    for i in range(2, n):  # 3번째 계단부터 dp점화식을 이용해서 풀기
+        dp[i] = max(dp[i-3] + score[i-1] + score[i],  # i-3까지의 계단 점수 최댓값과 i-1, i 계단의 합
+                    dp[i-2] + score[i])  # i-2까지의 계단 점수 최댓값과 i 계단의 합
     print(dp[-1])
