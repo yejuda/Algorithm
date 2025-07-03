4a8b6250b24e51e6e58e33d69c1c61d080b1b324
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-07-03 12:42:24 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,20 +1,15 @@
-import sys
-
-input = sys.stdin.readline
-
 t = int(input())
 
-for _ in range(t):
+for i in range(t):
     n = int(input())
 
-    # dp테이블 정의
-    dp = [0] * 12
+    dp = [0] * 11
 
     dp[1] = 1
     dp[2] = 2
     dp[3] = 4
 
     for i in range(4, 11):
-        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
     print(dp[n])
