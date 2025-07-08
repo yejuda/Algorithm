9d86a33302da0c5a83a4e0818de821ee3fa8a67d
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-07-08 10:57:03 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,10 +1,11 @@
 n = int(input())
 num = list(map(int, input().split()))
-dp = [1] * n  # 자기 자신 하나만 가지는 경우 1로 초기화
 
-for i in range(1, n):  # 자기 자신의 경우를 뺐기 때문에 n-1길이만큼 순회
+dp = [1] * n
+
+for i in range(n):
     for j in range(i):
         if num[j] < num[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
+            dp[i] = max(dp[i], dp[j] + 1)  # num[i]가 새로운 원소로 추가되므로 dp[j] + 1 
 
 print(max(dp))
