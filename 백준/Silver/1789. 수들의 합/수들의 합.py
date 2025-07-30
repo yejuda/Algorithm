e68f092605a2804632fc54938a7942537c0b1d07
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-07-30 12:16:17 +0900

    [Silver V] Title: 수들의 합, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,13 +1,13 @@
-s = int(input())
-total = 0
+S = int(input())
+
+current_sum = 0
 num = 1
 
-# 더한게 s보다 작거나 같을 때까지
-while total <= s:
-    total += num
+while True:
+    if current_sum + num > S:
+        break # 현재 합에 다음 숫자를 더했을 때, S를 초과할 경우 루프 종료
+    
+    current_sum += num
     num += 1
 
-if total > s:
-    print(num - 2)
-else:
-    print(num - 1)
+print(num-1)
