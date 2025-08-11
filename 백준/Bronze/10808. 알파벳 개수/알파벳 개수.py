342de803572313628c6a71ff3362415da8050fb8
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-08-11 09:51:07 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,8 @@
+x = input()
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+li = []
+for i in alpha:
+    li.append(x.count(i))
+
+print(*li)
