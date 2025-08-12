2cef6c795d97cf4203737b055a3ebdee91e9cdee
LEE YE JU <110321222+yejuda@users.noreply.github.com>
2025-08-12 16:11:56 +0900

    [Silver III] Title: 선분 위의 점, Time: 660 ms, Memory: 43576 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
@@ -5,49 +5,43 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 corr = sorted(map(int, input().split()))
 
-# B 이하 - A 미만  --> 이 범위에 점의 좌표 값이 몇개 있는가?
-# 두 인덱스의 차이가 [A, B] 범위에 속하는 점 개수이다.
 
-# 점들 중 a이상인 첫번째 점의 위치
-def lower_b(arr, target):
-    # 0부터 arr의 길이까지
-    low = 0
-    high = len(arr)
-    while low < high:
-        mid = (high + low) // 2
+# 선분 중 가장 작은 점의 인덱스 구하기(target 이상인 값의 시작점을 찾는다.)
+def lower(arr, target):
+    # 일단 arr의 절반 위치 인덱스 구해
+    start = 0
+    end = len(arr)
+
+    # start가 end보다 작을 때까지 반복(이분탐색~)
+    while start < end:
+        mid = (start + end) // 2
+        
         if arr[mid] < target:
-            low = mid + 1
+            start = mid + 1
         else:
-            high = mid
-    return low
+            end = mid
+    return start
 
 
-# 점들 중 b 초과인 첫번째 점의 위치(그래야 인덱스 상으로 범위 안에 속함)
-def upper_b(arr, target):
-    low = 0
-    high = len(arr)
-    while low < high:
-        mid = (high + low) // 2
+# 선분 중 가장 큰 점의 인덱스 구하기(target보다 큰 값들 중 가장 첫 번쨰 값을 찾는다.)
+def upper(arr, target):
+    # 일단 arr의 절반 위치 인덱스 구해
+    start = 0
+    end = len(arr)
+
+    # start가 end보다 작을 때까지 반복(이분탐색~)
+    while start < end:    
+        mid = (start + end) // 2
+
         if arr[mid] <= target:
-            low = mid + 1
+            start = mid + 1
         else:
-            high = mid
-    return low
+            end = mid
+    return start
 
 
 for _ in range(m):
     a, b = map(int, input().split())
-    left_idx = lower_b(corr, a)
-    right_idx = upper_b(corr, b)
+    left_idx = lower(corr, a)
+    right_idx = upper(corr, b)
     print(right_idx - left_idx)
-
-
-# for _ in range(m):
-#     cnt = 0
-#     a, b = map(int, input().split())
-
-#     for i in corr:
-#         if a <= i <= b:
-#             cnt += 1
-
-#     print(cnt)
